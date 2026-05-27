286f7642373a92f08b3ec7071971bd5163624109
mcwillytea-here <mcwillytea@gmail.com>
2026-05-27 20:30:57 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-1import { createClient } from '@/utils/supabase/server'
-2import { cookies } from 'next/headers'
 import Link from 'next/link';
 import { Skull } from 'lucide-react';
 
@@ -32,19 +30,3 @@ export default function HomePage() {
   );
 }
 
-
-
-4export default async function Page() {
-5  const cookieStore = await cookies()
-6  const supabase = createClient(cookieStore)
-7
-8  const { data: todos } = await supabase.from('todos').select()
-9
-10  return (
-11    <ul>
-12      {todos?.map((todo) => (
-13        <li key={todo.id}>{todo.name}</li>
-14      ))}
-15    </ul>
-16  )
-17}
